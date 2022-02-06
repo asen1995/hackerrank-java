9436c32935c77a7f6b1227358e4999131fd187ce
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-02-06 21:43:46 +0200

    basics

--- src/main/java/hackerRankJavaApplication.java
+++ src/main/java/hackerRankJavaApplication.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class hackerRankJavaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World.");
-		System.out.println("Hello, Java.");
+		Scanner scanner = new Scanner(System.in);
+		String myString = scanner.next();
+		int myInt = scanner.nextInt();
+		scanner.close();
+
+		System.out.println("myString is: " + myString);
+		System.out.println("myInt is: " + myInt);
 	}
 }
