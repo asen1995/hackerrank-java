5d632efecab4c9b491b02714ce4e592c5e2775f8
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-02-13 14:24:35 +0200

    format

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -4,19 +4,17 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class IntroductionChallenges {
-	
-	//Java Date and Time
-	public static String findDayOfWeek(int month, int day, int year) {
-		
 
-		String dayNames[] = {"Sunday", "Monday", "Tuesday", "Wednesday",
-            "Thursday", "Friday", "Saturday"};
-		//January is represented as 0 
-		month= month -1; 
+	// Java Date and Time
+	public static String findDayOfWeek(int month, int day, int year) {
+
+		String dayNames[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+		// January is represented as 0
+		month = month - 1;
 		Calendar myCalendar = new GregorianCalendar(year, month, day);
 
-		String dayString =  dayNames[myCalendar.getTime().getDay()];
-		
+		String dayString = dayNames[myCalendar.getTime().getDay()];
+
 		return dayString.toUpperCase();
 	}
 }
