3570fd04716d86084d925960690efbb1f4217b0f
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-02-22 20:12:45 +0200

    Lexicographical Order challenge

--- src/main/java/introduction/StringsChallenges.java
+++ src/main/java/introduction/StringsChallenges.java
@@ -14,4 +14,16 @@ public class StringsChallenges {
 		return value == 0;
 	}
 
+	
+	public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+	
 }
