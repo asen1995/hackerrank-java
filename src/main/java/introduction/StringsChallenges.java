579d64d4690286699e2249f8b0bbd255b392cd66
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-02-23 16:28:33 +0200

    formating

--- src/main/java/introduction/StringsChallenges.java
+++ src/main/java/introduction/StringsChallenges.java
@@ -14,29 +14,27 @@ public class StringsChallenges {
 		return value == 0;
 	}
 
-	
 	public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
-        
-        
-        int lenght = 0;
-        
-        smallest = s.substring( lenght, lenght + k );
-        lenght++;
-        
-        while((lenght + k)  < s.length() - 1 ) {
-        	String secondStringForCompare = s.substring(lenght, lenght + k);
-        	if(secondStringForCompare.compareTo(smallest) < 0 ) { //smaller
-        		smallest = secondStringForCompare;
-        	}
-        	lenght++;
-        }
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-        
-        return smallest + "\n" + largest;
-    }
-	
+		String smallest = "";
+		String largest = "";
+
+		int lenght = 0;
+
+		smallest = s.substring(lenght, lenght + k);
+		lenght++;
+
+		while ((lenght + k) < s.length() - 1) {
+			String secondStringForCompare = s.substring(lenght, lenght + k);
+			if (secondStringForCompare.compareTo(smallest) < 0) { // smaller
+				smallest = secondStringForCompare;
+			}
+			lenght++;
+		}
+		// Complete the function
+		// 'smallest' must be the lexicographically smallest substring of length 'k'
+		// 'largest' must be the lexicographically largest substring of length 'k'
+
+		return smallest + "\n" + largest;
+	}
+
 }
