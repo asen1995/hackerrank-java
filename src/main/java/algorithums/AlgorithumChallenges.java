d7598a17c37b7d9b4d04b5db894634dece642fca
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-12-04 14:43:59 +0200

    comparing triplets rating array and returning scores challenge

--- src/main/java/algorithums/AlgorithumChallenges.java
+++ src/main/java/algorithums/AlgorithumChallenges.java
@@ -1,5 +1,6 @@
 package algorithums;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class AlgorithumChallenges {
@@ -7,6 +8,19 @@ public class AlgorithumChallenges {
 
     public List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
 
-        return a;
+        if (a.size() != 3 || b.size() != 3) {
+            throw new IllegalArgumentException("the two arrays needs to have 3 elements each!");
+        }
+
+        int aRates = 0, bRates = 0;
+
+        for (int rateIndex = 0; rateIndex < 3; rateIndex++) {
+            if (a.get(rateIndex) > b.get(rateIndex)) {
+                aRates++;
+            } else if (a.get(rateIndex) < b.get(rateIndex)) {
+                bRates++;
+            }
+        }
+        return Arrays.asList(aRates, bRates);
     }
 }
--- src/test/java/algorithums/AlgorithumChallengesTest.java
+++ src/test/java/algorithums/AlgorithumChallengesTest.java
@@ -1,7 +1,5 @@
 package algorithums;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -12,18 +10,23 @@ import static org.junit.Assert.assertEquals;
 public class AlgorithumChallengesTest {
 
 
+    private final AlgorithumChallenges algorithumChallenges = new AlgorithumChallenges();
+
     @Test
     public void testCompareTriplets() {
-//        17 28 30
-//        99 16 8
-//
+
+        //        Sample Input
+        //        17 28 30
+        //        99 16 8
+
         final List<Integer> aliceRatings = Arrays.asList(17, 28, 30);
         final List<Integer> bobRatings = Arrays.asList(99, 16, 8);
-        AlgorithumChallenges algorithumChallenges = new AlgorithumChallenges();
         final List<Integer> result = algorithumChallenges.compareTriplets(aliceRatings, bobRatings);
 
-//        2 1
+
+        //      Sample Output
+        //        2 1
         assertEquals(result.get(0), Integer.valueOf(2));
-        assertEquals(result.get(2), Integer.valueOf(1));
+        assertEquals(result.get(1), Integer.valueOf(1));
     }
 }
