3a7460208439e7f3f8032d3fdd0b9db468bd2274
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-12-19 18:28:47 +0200

    hourglassSum in 2d array challenge

--- src/main/java/datastructures/DataStructureChallenges.java
+++ src/main/java/datastructures/DataStructureChallenges.java
@@ -5,9 +5,41 @@ import java.util.List;
 
 public class DataStructureChallenges {
 
+    public static int hourglassSum(List<List<Integer>> arr) {
+
+//        assertEquals(19, dataStructureChallenges.hourglassSum(Arrays.asList(
+//                Arrays.asList(1, 1, 1, 0, 0, 0),
+//                Arrays.asList(0, 1, 0, 0, 0, 0),
+//                Arrays.asList(1, 1, 1, 0, 0, 0),
+//                Arrays.asList(0, 0, 2, 4, 4, 0),
+//                Arrays.asList(0, 0, 0, 2, 0, 0),
+//                Arrays.asList(0, 0, 1, 2, 4, 0))));
+//
+        int maxSum = 0;
+
+        final int numberOfArrays = arr.size();
+
+        for (int i = 0; i < numberOfArrays - 2; i++) {
+
+            for (int j = 0; j < numberOfArrays - 2; j++) {
+                int sum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2)
+                        + arr.get(i + 1).get(j + 1) +
+                        arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+
+                if(sum > maxSum){
+                    maxSum = sum;
+                }
+            }
+
+        }
+
+
+        return maxSum;
+
+    }
+
     public List<Integer> reverseArray(List<Integer> a) {
         Collections.reverse(a);
         return a;
     }
-
 }
--- src/test/java/datastructures/DataStructureChallengesTest.java
+++ src/test/java/datastructures/DataStructureChallengesTest.java
@@ -15,4 +15,14 @@ public class DataStructureChallengesTest {
         assertEquals(Arrays.asList(2,3,4,1), dataStructureChallenges.reverseArray(Arrays.asList(1, 4, 3, 2)));
     }
 
+    @Test
+    public void hourglassSum() {
+        assertEquals(19, dataStructureChallenges.hourglassSum(Arrays.asList(
+                Arrays.asList(1, 1, 1, 0, 0, 0),
+                Arrays.asList(0, 1, 0, 0, 0, 0),
+                Arrays.asList(1, 1, 1, 0, 0, 0),
+                Arrays.asList(0, 0, 2, 4, 4, 0),
+                Arrays.asList(0, 0, 0, 2, 0, 0),
+                Arrays.asList(0, 0, 1, 2, 4, 0))));
+    }
 }
