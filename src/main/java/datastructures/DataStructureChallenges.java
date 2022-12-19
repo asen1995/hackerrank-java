db4dc338475fba6d9598466104c0e99e838dc7f0
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-12-19 18:01:55 +0200

    use collections.reverse cause it operates on the same list

--- src/main/java/datastructures/DataStructureChallenges.java
+++ src/main/java/datastructures/DataStructureChallenges.java
@@ -1,22 +1,13 @@
 package datastructures;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DataStructureChallenges {
 
     public List<Integer> reverseArray(List<Integer> a) {
-
-        List<Integer> reversed = new ArrayList<>();
-
-        for (int i = a.size() - 1; i >= 0; i--) {
-
-            reversed.add(a.get(i));
-        }
-
-        return reversed;
-
-
+        Collections.reverse(a);
+        return a;
     }
 
 }
