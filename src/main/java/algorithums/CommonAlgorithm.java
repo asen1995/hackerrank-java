ff5d7a8d46c0725c773a6b874c75f1306b712fa9
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-12-19 22:46:11 +0200

    common algorithms binary search

---
+++ src/main/java/algorithums/CommonAlgorithm.java
@@ -0,0 +1,31 @@
+package algorithums;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CommonAlgorithm {
+
+    public int binarySearch(int search, List<Integer> list) {
+
+        Collections.sort(list);
+
+        int left = 0, right = list.size() - 1;
+
+        while (left < right) {
+
+            int middleIndex = (right + left) / 2;
+            if (list.get(middleIndex) == search) {
+                return middleIndex;
+
+            } else if (list.get(middleIndex) > search) {
+                right = middleIndex;
+            } else {
+                left = middleIndex;
+            }
+
+
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/java/algorithums/CommonAlgorithmTest.java
@@ -0,0 +1,19 @@
+package algorithums;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CommonAlgorithmTest {
+
+    final CommonAlgorithm commonAlgorithms = new CommonAlgorithm();
+
+    @Test
+    public void binarySearch() {
+        assertEquals(8, commonAlgorithms.binarySearch(66, Arrays.asList(4, 66, 44, 33, 22, 77, 55, 2, 34, 37)));
+    }
+
+}
