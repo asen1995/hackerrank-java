b9bee8c5baf28241c3d5f6e21c917706f1afc39a
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-12-27 21:13:28 +0200

    linked list test modify existing

--- src/test/java/datastructures/DataStructuresTest.java
+++ src/test/java/datastructures/DataStructuresTest.java
@@ -1,14 +1,17 @@
 package datastructures;
 
 import org.junit.Test;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataStructuresTest {
 
     @Test
-    public void arrayListTest(){
+    public void arrayListTest() {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -20,5 +23,20 @@ public class DataStructuresTest {
         assertEquals(3, list.get(2).intValue());
 
     }
+
+    @Test
+    public void linkedListTest() {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.add(1, 22);
+        assertEquals(22, list.get(1).intValue());
+        ((LinkedList<Integer>) list).addFirst(33);
+
+        assertEquals(33, list.get(0).intValue());
+    }
+
 
 }
