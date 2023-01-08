bd50586dfee2e98dc8eb9af8fcfbc00959c6310d
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-08 14:52:19 +0200

    compareLists iterative approach

--- src/main/java/datastructures/DataStructureChallenges.java
+++ src/main/java/datastructures/DataStructureChallenges.java
@@ -292,19 +292,25 @@ public class DataStructureChallenges {
     }
 
     public boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-        if(head1 == null && head2 == null){
-            return true;
-        }
 
-        if(head1 == null || head2 == null){
-            return false;
-        }
+        while(true){
 
-        if(head1.data != head2.data){
-            return false;
-        }
+            if(head1 == null && head2 == null){
+                return true;
+            }
 
-        return compareLists(head1.next,head2.next);
+            if(head1 == null || head2 == null){
+                return false;
+            }
+
+            if(head1.data != head2.data){
+                return false;
+            }
+
+            head1 = head1.next;
+            head2 = head2.next;
+
+        }
 
     }
 
