ec3473ba9806bc6bb900a4a830ecc4999377001f
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-08 14:41:34 +0200

    compareLists challenge recursive approach

--- src/main/java/datastructures/DataStructureChallenges.java
+++ src/main/java/datastructures/DataStructureChallenges.java
@@ -291,4 +291,21 @@ public class DataStructureChallenges {
         return currentHead;
     }
 
+    public boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        if(head1 == null && head2 == null){
+            return true;
+        }
+
+        if(head1 == null || head2 == null){
+            return false;
+        }
+
+        if(head1.data != head2.data){
+            return false;
+        }
+
+        return compareLists(head1.next,head2.next);
+
+    }
+
 }
