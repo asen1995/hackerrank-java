7564daf6f49507fc741f124d7f4cf5e546b1d15c
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-11 22:00:58 +0200

    online test fix errors

--- src/test/java/online/OnlineChallengeTest.java
+++ src/test/java/online/OnlineChallengeTest.java
@@ -6,19 +6,20 @@ import static org.junit.Assert.*;
 
 public class OnlineChallengeTest {
 
+    private OnlineChallenge onlineChallenge = new OnlineChallenge();
 
     @Test
     public void arrayOfArrayProducts() {
         int[] input = {8, 10, 2};
         int[] expected = {20, 16, 80};
-        assertArrayEquals(expected, dataStructureChallenges.arrayOfArrayProducts(input));
+        assertArrayEquals(expected, onlineChallenge.arrayOfArrayProducts(input));
     }
 
     @Test
     public void moveZeroesToEndOfArray() {
         int[] input = {0, 3, 1, 0, -2};
         int[] expected = {3, 1, -2, 0, 0};
-        assertArrayEquals(expected, dataStructureChallenges.moveZeroesToEndOfArray(input));
+        assertArrayEquals(expected, onlineChallenge.moveZeroesToEndOfArray(input));
     }
 
 }
