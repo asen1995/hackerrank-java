166949b8efad36ff8bb29080e1e5f3dadca12920
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-11 16:50:23 +0200

    reverseString with recursion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,40 @@
-import introduction.StringsChallenges;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		// asen   - nesa
+
+		//
+
+		// a
+		// s + a
+		// e + s + a
+		// n + e + s + a
+
+		String result = reverseString("asen");
+		System.out.println(result);
 	
+	}
+
+
+	public static String reverseString(String param){
+
+		if(param.length() == 1){
+			return param;
+		}else {
+			final String s = reverseString(param.substring(1, param.length()));
+			return s.concat(String.valueOf(param.charAt(0)));
+		}
+
+		// e s a
+		// sa
+		// a
+
+
+		//concat 1
+
+
+
 	}
 
 }
