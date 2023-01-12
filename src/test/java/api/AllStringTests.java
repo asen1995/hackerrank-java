b8221c861ec1b12578ac2414a0ccd29606fdc96a
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-13 01:18:01 +0200

    length

---
+++ src/test/java/api/AllStringTests.java
@@ -0,0 +1,14 @@
+package api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AllStringTests {
+
+    @Test
+    public void allStringMethodsTest() {
+        assertEquals("something".length(), 9);
+    }
+
+}
