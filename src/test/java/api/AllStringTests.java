7f2c6726968d12c4bd606af5fa3a16001878e750
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-13 01:23:12 +0200

    substring by one or two indexes

--- src/test/java/api/AllStringTests.java
+++ src/test/java/api/AllStringTests.java
@@ -14,6 +14,13 @@ public class AllStringTests {
         assertEquals("something".charAt(1), 'o');
         assertEquals("something".charAt(2), 'm');
 
+        assertEquals("something".substring(0, 3), "som");
+        assertEquals("something".substring(3, 9), "ething");
+
+        assertEquals("something".substring(3), "ething");
+        assertEquals("something".substring(0), "something");
+        assertEquals("something".substring(5), "hing");
+
     }
 
 }
