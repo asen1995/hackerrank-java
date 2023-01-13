712927a932256c3f0b309e40d40d4d9b176a2b72
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-13 20:01:33 +0200

    findDuplicates using binary search

--- src/main/java/online/OnlineChallenge.java
+++ src/main/java/online/OnlineChallenge.java
@@ -1,5 +1,8 @@
 package online;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OnlineChallenge {
 
     static int[] arrayOfArrayProducts(int[] arr) {
@@ -45,5 +48,53 @@ public class OnlineChallenge {
         }
 
         return arr;
+    }
+
+    static int[] findDuplicates(int[] arr1, int[] arr2) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i < arr1.length ; i++) {
+
+            if(binarySearch(arr1[i] , arr2) != -1 ){
+                result.add(arr1[i]);
+            }
+
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    static int binarySearch(int target, int[] arr) {
+
+        int begin = 0;
+        int end = arr.length;
+
+        while(begin <= end){
+
+            int middle = ( begin + end ) / 2;
+
+            if(arr[middle] == target){
+                return middle;
+            }else if ( target > arr[middle] ){
+                begin = middle + 1;
+            }else {
+                end = middle - 1;
+            }
+
+
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 5, 6, 7};
+        int[] arr2 = {3, 6, 7, 8, 20};
+        int[] result = findDuplicates(arr1, arr2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
     }
 }
