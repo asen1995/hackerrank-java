547f2200d5c76d666587b7a188cfabd00e790f63
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-15 14:29:31 +0200

    Application class loader and Bootstrap class loader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import datastructures.DataStructureChallenges;
+
 public class Main {
 
 	public static void main(String[] args) {
+
+		Class c = Main.class;
+		System.out.println(c.getClassLoader());
+
+		System.out.println(DataStructureChallenges.class.getClassLoader());
+		System.out.println(Object.class.getClassLoader());
 	}
-
-
 
 }
