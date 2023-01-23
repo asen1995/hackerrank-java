2794dc97e572ebf47b9cc3215189777dcfe2ef8f
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-23 13:18:37 +0200

    clean

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Class c = Main.class;
-		System.out.println(c.getClassLoader());
-
-		System.out.println(DataStructureChallenges.class.getClassLoader());
-		System.out.println(Object.class.getClassLoader());
 	}
+
 
 }
