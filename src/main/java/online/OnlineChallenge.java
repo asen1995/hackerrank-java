61703ae5106e123b8b51f4310659a4e40ddddd65
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-24 15:21:30 +0200

    Revert "mergeSortedArrays implementation"
    This reverts commit 2f1dc95268a8a5f5c8156d60d46c1be5c445c90e.

--- src/main/java/online/OnlineChallenge.java
+++ src/main/java/online/OnlineChallenge.java
@@ -253,39 +253,4 @@ public class OnlineChallenge {
         }
     }
 
-
-    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
-
-        int[] result = new int[arr1.length + arr2.length];
-
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        while (i < arr1.length && j < arr2.length) {
-
-            if (arr1[i] < arr2[j]) {
-                result[k] = arr1[i];
-                i++;
-            } else {
-                result[k] = arr2[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < arr1.length) {
-            result[k] = arr1[i];
-            i++;
-            k++;
-        }
-
-        while (j < arr2.length) {
-            result[k] = arr2[j];
-            j++;
-            k++;
-        }
-
-        return result;
-    }
 }
