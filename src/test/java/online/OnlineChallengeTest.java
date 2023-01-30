6a614ca4d2b8c9795dd012bd4c61ad312588a0d6
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-30 19:16:55 +0200

    format

--- src/test/java/online/OnlineChallengeTest.java
+++ src/test/java/online/OnlineChallengeTest.java
@@ -21,10 +21,11 @@ public class OnlineChallengeTest {
         int[] expected = {3, 3, 1, -2, 0};
         assertArrayEquals(expected, onlineChallenge.moveZeroesToEndOfArray(input));
     }
+
     @Test
     public void moveZeroesToEndOfArraySecondExample() {
-        int[] input = {0 ,0 ,3 ,4 ,0 ,4 ,2 };
-        int[] expected = {3 ,4 ,4 ,2 ,0 ,0 ,0 };
+        int[] input = {0, 0, 3, 4, 0, 4, 2};
+        int[] expected = {3, 4, 4, 2, 0, 0, 0};
         assertArrayEquals(expected, onlineChallenge.moveZeroesToEndOfArray(input));
     }
 
