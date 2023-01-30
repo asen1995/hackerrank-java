4f3cef8fee0956a3c37c96d271131a206fe5ce16
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-30 19:16:08 +0200

    AESEncryption algorithm

---
+++ src/main/java/online/AESEncryption.java
@@ -0,0 +1,28 @@
+package online;
+
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESEncryption {
+    private static final String ALGORITHM = "AES";
+    private static final String key = "AESEncryptionKey";
+
+    public static String encrypt(String value) throws Exception {
+        Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedValue = cipher.doFinal(value.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedValue);
+    }
+
+    public static String decrypt(String value) throws Exception {
+        Key secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedValue = Base64.getDecoder().decode(value);
+        byte[] decryptedValueBytes = cipher.doFinal(decryptedValue);
+        return new String(decryptedValueBytes);
+    }
+}
