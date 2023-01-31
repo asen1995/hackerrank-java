7e45ca1c310a12d091b75c60df939f4fe8de65f5
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-31 18:13:06 +0200

    move performance test of strings to method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,56 +2,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
-		final int n = 100000;
-
-		long start = System.currentTimeMillis();
-
-		String string = "a";
-		for(int i = 0; i< n; i++) {
-			string = string + "a";
-		}
-
-		long end = System.currentTimeMillis();
-		System.out.println("String concatenation took " + (end-start) + " ms");
-
-
-
-		String formatString = "a%s";
-
-		start = System.currentTimeMillis();
-
-		for(int i = 0; i< n; i++) {
-			formatString = String.format(formatString, "a%s");
-		}
-
-		end = System.currentTimeMillis();
-
-		System.out.println("String format took " + (end-start) + " ms");
-
-
-		StringBuilder sb = new StringBuilder("a");
-		start = System.currentTimeMillis();
-
-		for(int i = 0; i< n; i++) {
-			sb.append("a");
-		}
-
-		end = System.currentTimeMillis();
-		System.out.println("StringBuilder took " + (end-start) + " ms");
-
-
-		StringBuffer sbf = new StringBuffer("a");
-		start = System.currentTimeMillis();
-
-		for(int i = 0; i< n; i++) {
-			sbf.append("a");
-		}
-
-		end = System.currentTimeMillis();
-		System.out.println("StringBuffer took " + (end-start) + " ms");
-
 	}
-
 
 }
