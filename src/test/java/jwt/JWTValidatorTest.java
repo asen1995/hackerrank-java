5453544278bc5ff2f6a51ef449caff4cc6894529
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-31 22:35:14 +0200

    extractHMAC256JWTClaims test

--- src/test/java/jwt/JWTValidatorTest.java
+++ src/test/java/jwt/JWTValidatorTest.java
@@ -1,7 +1,11 @@
 package jwt;
 
+import com.auth0.jwt.interfaces.Claim;
 import org.junit.Test;
 
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class JWTValidatorTest {
@@ -22,4 +26,17 @@ public class JWTValidatorTest {
 
     }
 
+    @Test
+    public void extractHMAC256JWTClaimsTest() {
+        String secret = "asen-secret";
+
+        JWTValidator jwtValidator = new JWTValidator();
+
+        JWTgenerator jwtGenerator = new JWTgenerator();
+
+        String token = jwtGenerator.generateHMAC256JWT(secret);
+
+        Map<String, Claim> jwtClaims = jwtValidator.extractHMAC256JWTClaims(token, secret);
+        assertEquals("asen", jwtClaims.get("iss").asString());
+    }
 }
