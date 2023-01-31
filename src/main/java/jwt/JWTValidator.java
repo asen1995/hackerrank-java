c1b6fe1ff3d6168bc9f3b050664a090651b097ab
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-31 22:34:50 +0200

    extractHMAC256JWTClaims impl

--- src/main/java/jwt/JWTValidator.java
+++ src/main/java/jwt/JWTValidator.java
@@ -2,7 +2,10 @@ package jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Map;
 
 public class JWTValidator {
     public boolean validateHMAC256JWT(String token, String secret) {
@@ -18,5 +21,22 @@ public class JWTValidator {
         }
 
         return true;
+    }
+
+    public Map<String, Claim> extractHMAC256JWTClaims(String token, String secret) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("asen-secret");
+            DecodedJWT jwt = JWT.require(algorithm).build().verify(token);
+
+            System.out.println("JWT verification succeeded!");
+
+            return jwt.getClaims();
+
+        } catch (Exception e) {
+            System.out.println("JWT verification failed!");
+            throw new RuntimeException("JWT verification failed!");
+        }
+
+
     }
 }
