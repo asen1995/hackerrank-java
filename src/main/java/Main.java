c39c8084a7483cd34a0467306bfe81436d071a2c
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-31 18:30:21 +0200

    clear

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,8 @@
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import jwt.JWTgenerator;
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		JWTgenerator jwtGenerator = new JWTgenerator();
-		String token = jwtGenerator.generateHMAC256JWT("asen-secret");
 
-		try {
-			Algorithm algorithm = Algorithm.HMAC256("asen-secret");
-			DecodedJWT jwt = JWT.require(algorithm).build().verify(token);
-			System.out.println("JWT verification succeeded!");
-		} catch (Exception e) {
-			System.out.println("JWT verification failed!");
-		}
 
 	}
 
