77e18864c19f88cd2d463bd0ddd85421947bbdb0
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-02-05 18:16:17 +0200

    conversion to dto performance check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 
+		List<Person> persons = new ArrayList<>();
 
+		for(int i = 0; i < 1_000_000; i++) {
+			persons.add(new Person("Name" + i, i));
+		}
+
+
+		long currentTime = System.currentTimeMillis();
+
+		List<PersonDTo> personDTos = persons.stream()
+				.map(person -> new PersonDTo(person.getName(), person.getAge()))
+				.collect(Collectors.toList());
+
+		System.out.println("Stream took: " + (System.currentTimeMillis() - currentTime) + " ms");
+
+
+		currentTime = System.currentTimeMillis();
+
+		List<PersonDTo> personDTos2 = new ArrayList<>();
+
+		for(Person person : persons) {
+			personDTos2.add(new PersonDTo(person.getName(), person.getAge()));
+		}
+
+		System.out.println("For loop took: " + (System.currentTimeMillis() - currentTime) + " ms");
 	}
 
+	static class Person {
+		private String name;
+		private int age;
+
+		public Person(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+	}
+
+	static class PersonDTo {
+		private String name;
+		private int age;
+
+		public PersonDTo(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+	}
 }
