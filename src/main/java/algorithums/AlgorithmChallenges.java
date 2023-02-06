cae8c74703cf8fdc0d2e610cf576eb98430b822b
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-02-06 13:30:51 +0200

    change return type

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -464,7 +464,7 @@ public class AlgorithmChallenges {
         return Math.min(numberOfTurnsFromFront, numberOfTurnsFromBack);
     }
 
-    public void selectionSort(int[] arr) {
+    public int[] selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
@@ -476,5 +476,7 @@ public class AlgorithmChallenges {
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
+
+        return arr;
     }
 }
