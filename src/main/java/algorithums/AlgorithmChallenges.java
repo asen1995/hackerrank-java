7d4065c1f6efa1360501f2daa14343988ad043cb
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-02-06 11:52:08 +0200

    selection sort impl

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -463,4 +463,18 @@ public class AlgorithmChallenges {
 
         return Math.min(numberOfTurnsFromFront, numberOfTurnsFromBack);
     }
+
+    public void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
 }
