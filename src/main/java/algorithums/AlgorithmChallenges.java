65fa7dcb90b0848ff5bc9a96f0c8572b9482b2bc
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-02-07 16:20:55 +0200

    squash test

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -465,13 +465,18 @@ public class AlgorithmChallenges {
     }
 
     public int[] selectionSort(int[] arr) {
+
         for (int i = 0; i < arr.length; i++) {
+
             int minIndex = i;
+
             for (int j = i + 1; j < arr.length; j++) {
+
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
+
             int temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
