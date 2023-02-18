b2354c14f0860437ff89f87771aa61d6ea1958d3
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-02-18 17:46:34 +0200

    getArrayWithNumbers

--- src/main/java/introduction/StringsChallenges.java
+++ src/main/java/introduction/StringsChallenges.java
@@ -185,4 +185,15 @@ public class StringsChallenges {
     }
 
 
+    public int[] getArrayWithNumbers(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+
+
+
 }
