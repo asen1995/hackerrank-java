5296a8cdee5dd1ccfdfa8f31e2d252027ce68b1e
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-11 18:32:18 +0200

    Revert "remove mirror"
    This reverts commit b8e51f192d8dea7ae6f3f72410ca6dcac33d3dcb.

--- src/main/java/online/OnlineChallenge.java
+++ src/main/java/online/OnlineChallenge.java
@@ -50,4 +50,52 @@ public class OnlineChallenge {
     }
 
 
+    static char[] reverseWordsMirror(char[] arr) {
+
+        mirror(0,arr.length - 1 , arr);
+
+        int wordBegin = 0;
+        int endBegin = 0;
+
+        while (wordBegin < arr.length &&  arr[wordBegin] == ' '){
+            wordBegin++;
+        }
+
+        for(int i = wordBegin; i < arr.length ; i++){
+
+            if(arr[i] == ' ' ){
+                endBegin = i - 1;
+
+                mirror(wordBegin, endBegin,arr);
+
+                wordBegin = i;
+
+                while(i < arr.length && arr[i] == ' ' ){
+                    i++;
+                    wordBegin++;
+                }
+            }
+
+        }
+
+        if(wordBegin < arr.length){
+            mirror(wordBegin, arr.length - 1,arr);
+        }
+
+        return arr;
+    }
+
+    private static void mirror(int begin, int end, char[] arr) {
+
+        while(begin < end){
+
+            char temp = arr[begin];
+            arr[begin] = arr[end];
+            arr[end] = temp;
+
+            begin++;
+            end--;
+        }
+    }
+
 }
