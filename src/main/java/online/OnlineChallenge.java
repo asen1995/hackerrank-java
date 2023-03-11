fa8f0845b8f2372afe1a76c9f3c6c9ef3e691b77
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-12 00:30:41 +0200

    findTwoSum

--- src/main/java/online/OnlineChallenge.java
+++ src/main/java/online/OnlineChallenge.java
@@ -202,4 +202,22 @@ public class OnlineChallenge {
         return result;
     }
 
+    public static int[] findTwoSum(int[] arr, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
 }
