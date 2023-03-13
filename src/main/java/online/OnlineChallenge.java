8c28ec7a0e95af88cb6f0f4440cc40fbb35988f5
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-13 10:01:12 +0200

    findTwoSumWithHash

--- src/main/java/online/OnlineChallenge.java
+++ src/main/java/online/OnlineChallenge.java
@@ -220,4 +220,23 @@ public class OnlineChallenge {
 
         return result;
     }
+
+    public static int[] findTwoSumWithHash(int[] arr, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
 }
