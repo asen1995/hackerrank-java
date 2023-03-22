545e618b6fcb75a2b2136cad60087b9fef0e171f
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-23 00:02:25 +0200

    heapSort

--- src/test/java/api/SortingAlgorithms.java
+++ src/test/java/api/SortingAlgorithms.java
@@ -128,4 +128,37 @@ public class SortingAlgorithms {
         }
         return result;
     }
+
+
+    public static int[] heapSort(int[] array) {
+        int numberOfElements = array.length;
+        for (int i = numberOfElements / 2 - 1; i >= 0; i--) {
+            heapify(array, numberOfElements, i);
+        }
+        for (int i = numberOfElements - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+        return array;
+    }
+
+    private static void heapify(int[] array, int numberOfElements, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < numberOfElements && array[left] > array[largest]) {
+            largest = left;
+        }
+        if (right < numberOfElements && array[right] > array[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+            heapify(array, numberOfElements, largest);
+        }
+    }
 }
