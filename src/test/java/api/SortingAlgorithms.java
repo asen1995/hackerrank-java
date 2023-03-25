625c67d23a9849d230d393237a9c96b6c94a843f
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-25 05:18:54 +0200

    Revert "countingSort"
    This reverts commit 9ebe96f9db33a6d09bfeeb9bd6164ac671e39c72.

--- src/test/java/api/SortingAlgorithms.java
+++ src/test/java/api/SortingAlgorithms.java
@@ -161,27 +161,4 @@ public class SortingAlgorithms {
             heapify(array, numberOfElements, largest);
         }
     }
-
-    public static int[] countingSort(int[] array) {
-        int numberOfElements = array.length;
-        int max = array[0];
-        for (int i = 1; i < numberOfElements; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
-        }
-        int[] count = new int[max + 1];
-        for (int i = 0; i < numberOfElements; i++) {
-            count[array[i]]++;
-        }
-        int[] result = new int[numberOfElements];
-        int resultIndex = 0;
-        for (int i = 0; i <= max; i++) {
-            for (int j = 0; j < count[i]; j++) {
-                result[resultIndex] = i;
-                resultIndex++;
-            }
-        }
-        return result;
-    }
 }
