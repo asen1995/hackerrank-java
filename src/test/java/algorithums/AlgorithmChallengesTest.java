9a0d226575ad34fbc93f5d5e71924f2162ca777e
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-04-01 00:14:57 +0300

    Revert "Revert "binary search test""
    This reverts commit 4f08ca7c97c42a7dea5eaa059e1476ddef9635b6.

--- src/test/java/algorithums/AlgorithmChallengesTest.java
+++ src/test/java/algorithums/AlgorithmChallengesTest.java
@@ -253,4 +253,9 @@ public class AlgorithmChallengesTest {
     public void reverseString() {
         assertEquals("olleh", algorithmChallenges.reverseString("hello"));
     }
+
+    @Test
+    public void binarySearch() {
+        assertEquals(3, algorithmChallenges.binarySearch(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 4));
+    }
 }
