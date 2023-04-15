aef410375efd5dc002faaf3ec1e35ec4f4eecca0
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-04-16 02:41:37 +0300

    Revert "sum shorts added"
    This reverts commit 5f80916b0329a4807e144dd8d26b31cdcfa2b215.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -64,10 +64,4 @@ public class IntroductionChallenges {
 	public static double devide(double number1,double number2) {
 		return number1/number2;
 	}
-
-
-	public short sum(short a, short b) {
-		return (short) (a + b);
-	}
-
 }
