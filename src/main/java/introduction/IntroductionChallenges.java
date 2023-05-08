4fb61c036621883fab84cd9fc706b5e52ff4f6f5
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-05-08 07:49:54 +0300

    adjacentElementsProduct

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -129,4 +129,17 @@ public class IntroductionChallenges {
 
         return result;
     }
+
+    public short adjacentElementsProduct(int[] inputArray) {
+
+        short result = Short.MIN_VALUE;
+
+        for (int i = 0; i < inputArray.length - 1; i++) {
+            if (inputArray[i] * inputArray[i + 1] > result) {
+                result = (short) (inputArray[i] * inputArray[i + 1]);
+            }
+        }
+
+        return result;
+    }
 }
