96207c2992194034d25fcc8e68324a5140022565
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-05-15 09:50:02 +0300

    findSecondLargest

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -129,4 +129,20 @@ public class IntroductionChallenges {
 
         return result;
     }
+
+    int findSecondLargest(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
+            }
+        }
+
+        return secondMax;
+    }
 }
