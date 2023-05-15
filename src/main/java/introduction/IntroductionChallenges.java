d4ed303dbca5cd93d1e66f2321eddfbe26895896
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-05-15 09:49:32 +0300

    Revert "makeArrayConsecutive"
    This reverts commit 143c6aa309f01b4f20e49f67e73cab82225caaab.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -146,20 +146,4 @@ public class IntroductionChallenges {
     public int shapeArea(int n) {
         return n*n + (n-1)*(n-1);
     }
-
-    public int makeArrayConsecutive2(int[] statues) {
-        int min = statues[0];
-        int max = statues[0];
-
-        for(int i = 1; i < statues.length; i++){
-            if(statues[i] < min){
-                min = statues[i];
-            }
-            if(statues[i] > max){
-                max = statues[i];
-            }
-        }
-
-        return max - min - statues.length + 1;
-    }
 }
