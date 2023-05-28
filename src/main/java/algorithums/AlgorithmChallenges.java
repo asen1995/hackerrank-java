4be7b4bbac005ea77540f0d119c02213f44529bb
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-05-28 18:49:14 +0300

    lca challenge

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -620,4 +620,20 @@ public class AlgorithmChallenges {
         }
         return palindrome;
     }
+
+
+    public static Node lca(Node root, int v1, int v2) {
+
+            if (root == null) return null;
+
+            if (root.data > v1 && root.data > v2) {
+                return lca(root.left, v1, v2);
+            }
+
+            if (root.data < v1 && root.data < v2) {
+                return lca(root.right, v1, v2);
+            }
+
+            return root;
+    }
 }
