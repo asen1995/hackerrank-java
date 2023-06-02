5bc0c7dc223659243dde9707daacc9e0f8d213bd
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-02 12:07:54 +0300

    Revert "remove"
    This reverts commit f293c678a0dab8a5fd8e3046457364a6a347a44c.

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -609,6 +609,22 @@ public class AlgorithmChallenges {
     }
 
 
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+
     public String buildPalindrome(String st) {
         String reverse = new StringBuilder(st).reverse().toString();
         String palindrome = st;
