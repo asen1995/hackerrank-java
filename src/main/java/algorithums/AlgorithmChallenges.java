ff7027ccc40dea16a83dd40adedcb56848724f53
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-02 12:07:50 +0300

    Revert "findMissingNumber"
    This reverts commit 89dc225145c7e8d360628fcf781405525c86a2f6.

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -622,13 +622,18 @@ public class AlgorithmChallenges {
     }
 
 
-    int findMissingNumber(int[] arr){
-        int n = arr.length + 1;
-        int sum = n * (n + 1) / 2;
-        int arrSum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            arrSum += arr[i];
-        }
-        return sum - arrSum;
+    public static Node lca(Node root, int v1, int v2) {
+
+            if (root == null) return null;
+
+            if (root.data > v1 && root.data > v2) {
+                return lca(root.left, v1, v2);
+            }
+
+            if (root.data < v1 && root.data < v2) {
+                return lca(root.right, v1, v2);
+            }
+
+            return root;
     }
 }
