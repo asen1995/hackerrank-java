8f3cd907d7cc4561b416a42cf6789e02d1362708
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-10 11:17:39 +0300

    n factorial

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -3,4 +3,11 @@ package introduction;
 public class IntroductionChallenges {
 
 
+    int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+
+    }
 }
