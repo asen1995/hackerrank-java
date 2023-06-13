88b09fae489a1866bcb72f77697ef555387155c2
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-13 21:15:59 +0300

    Revert "fibonacci"
    This reverts commit a6df6d31d98f0ea4effdd40529e11fac2fadb938.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -10,15 +10,4 @@ public class IntroductionChallenges {
         return n * factorial(n - 1);
 
     }
-
-    int fibonacci(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if(n == 1){
-            return 1;
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2);
-
-    }
 }
