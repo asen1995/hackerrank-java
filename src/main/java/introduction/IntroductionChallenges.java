5fe10c65ac59a6b75720bbad484ba66b1b0b937f
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-13 21:16:06 +0300

    Revert "Revert "isItValidBracket implementation""
    This reverts commit 4f3cb4ce5b06384d27758a8a0d79d82208da219a.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -1,7 +1,57 @@
 package introduction;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class IntroductionChallenges {
 
+
+    private static boolean isItValidBracket(String input) {
+
+        List<Character> openingBrackets = new LinkedList<>();
+
+        for (Character character : input.toCharArray()) {
+
+            switch (character) {
+                case ')': {
+                    if (openingBrackets.isEmpty()) return false;
+                    removeFromTailIfExists('(', openingBrackets);
+                    break;
+                }
+                case ']': {
+
+                    if (openingBrackets.isEmpty()) return false;
+                    removeFromTailIfExists('[', openingBrackets);
+                    break;
+                }
+                case '}': {
+                    if (openingBrackets.isEmpty()) return false;
+                    removeFromTailIfExists('{', openingBrackets);
+                    break;
+                }
+                default: {
+                    openingBrackets.add(character);
+                }
+
+            }
+
+
+        }
+
+        if (!openingBrackets.isEmpty()) return false;
+
+        return true;
+    }
+
+    private static void removeFromTailIfExists(Character closingBracket, List<Character> openingBrackets) {
+        if (openingBrackets.get(openingBrackets.size() - 1).equals(closingBracket)) {
+            openingBrackets.remove(openingBrackets.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isItValidBracket("()[]{}"));
+    }
 
     int factorial(int n) {
         if (n == 1) {
@@ -15,10 +65,12 @@ public class IntroductionChallenges {
         if (n == 0) {
             return 0;
         }
-        if(n == 1){
+        if (n == 1) {
             return 1;
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
 
     }
+
+
 }
