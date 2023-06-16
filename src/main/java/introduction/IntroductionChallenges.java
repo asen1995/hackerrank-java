14842b0aa14121d4603a02a4709191bd72e4196d
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-16 11:58:48 +0300

    power challenge

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -81,4 +81,12 @@ public class IntroductionChallenges {
     }
 
 
+    int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        return base * power(base, exponent - 1);
+
+    }
+
 }
