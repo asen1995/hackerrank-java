ce52c02f8cca84e08775739759d76f2de07b0b54
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-18 13:57:52 +0300

    remove

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -90,15 +90,4 @@ public class IntroductionChallenges {
     }
 
 
-    int gcd(int a, int b) {
-        if (a == b) {
-            return a;
-        }
-        if (a > b) {
-            return gcd(a - b, b);
-        }
-        return gcd(a, b - a);
-
-    }
-
 }
