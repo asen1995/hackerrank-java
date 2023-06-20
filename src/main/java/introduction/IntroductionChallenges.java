28a1326af6cf1e4ba90afb5c881ef76477793134
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-20 15:45:54 +0300

    Revert "remove"
    This reverts commit ce52c02f8cca84e08775739759d76f2de07b0b54.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -90,4 +90,15 @@ public class IntroductionChallenges {
     }
 
 
+    int gcd(int a, int b) {
+        if (a == b) {
+            return a;
+        }
+        if (a > b) {
+            return gcd(a - b, b);
+        }
+        return gcd(a, b - a);
+
+    }
+
 }
