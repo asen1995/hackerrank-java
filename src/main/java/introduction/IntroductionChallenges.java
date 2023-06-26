27fb2fa2f040e6d76535ea2f9a1d5c5e9531395c
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-26 09:56:57 +0300

    reverse

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -98,6 +98,29 @@ public class IntroductionChallenges {
         return true;
     }
 
+    public int reverse(int x) {
+
+        int reversed = 0;
+
+        while (x != 0) {
+
+            int pop = x % 10;
+            x /= 10;
+
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + pop;
+
+        }
+
+        return reversed;
+    }
 
     int factorial(int n) {
         if (n == 1) {
