d7a0242e7a06228eeff58a47ae5b583cda3251be
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-06-28 17:44:09 +0300

    Revert "myAtoi"
    This reverts commit 9ef62bec613e9fbe52e8f52390f01f86f16a729e.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -122,46 +122,6 @@ public class IntroductionChallenges {
         return reversed;
     }
 
-    public int myAtoi(String s) {
-
-        int index = 0;
-        int sign = 1;
-        int total = 0;
-
-        //1. Empty string
-        if (s.length() == 0) return 0;
-
-        //2. Remove Spaces
-        while (index < s.length() && s.charAt(index) == ' ')
-            index++;
-
-        if (index == s.length()) return 0;
-
-        //3. Handle signs
-        if (s.charAt(index) == '+' || s.charAt(index) == '-') {
-            sign = s.charAt(index) == '+' ? 1 : -1;
-            index++;
-        }
-
-        //4. Convert number and avoid overflow
-        while (index < s.length()) {
-
-            int digit = s.charAt(index) - '0';
-
-            if (digit < 0 || digit > 9) break;
-
-            //check if total will be overflow after 10 times and add digit
-            if (Integer.MAX_VALUE / 10 < total ||
-                    Integer.MAX_VALUE / 10 == total && Integer.MAX_VALUE % 10 < digit)
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-
-            total = 10 * total + digit;
-            index++;
-        }
-
-        return total * sign;
-    }
-
     int factorial(int n) {
         if (n == 1) {
             return 1;
