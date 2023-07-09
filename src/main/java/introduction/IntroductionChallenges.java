7413d8ceaa1b8edd3549aaefac5f0f1554258555
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-07-09 12:25:25 +0300

    Revert "gcd"
    This reverts commit dbc8bbe7754cbca98bc3a47c4692f559cfe2c56f.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -105,14 +105,4 @@ public class IntroductionChallenges {
 
     }
 
-    int gcd(int a, int b) {
-        if (a == b) {
-            return a;
-        }
-        if (a > b) {
-            return gcd(a - b, b);
-        }
-        return gcd(a, b - a);
-
-    }
 }
