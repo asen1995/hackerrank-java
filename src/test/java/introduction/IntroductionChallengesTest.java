7e245925c74a2f44ef590b95f329fb9f11112e52
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-07-09 12:25:22 +0300

    Revert "delete class"
    This reverts commit dea54ff359697a3a35288e391355034054de3a8f.

---
+++ src/test/java/introduction/IntroductionChallengesTest.java
@@ -0,0 +1,33 @@
+//package introduction;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+//
+//public class IntroductionChallengesTest {
+//
+//	@Test
+//	public void findDayOfWeek() {
+//		String day = IntroductionChallenges.findDayOfWeek(8, 14, 2017);
+//
+//		assertTrue(day.equals("MONDAY"));
+//
+//		String test2 = IntroductionChallenges.findDayOfWeek(2, 6, 2022);
+//
+//		assertTrue(test2.equals("SUNDAY"));
+//
+//	}
+//
+//	@Test
+//	public void currencyConverter() {
+//		double payment = 12324.134;
+//		assertTrue(IntroductionChallenges.currencyConverter(payment).equals("SUCCESS"));
+//	}
+//
+////	@Test
+////	public void calculator() {
+////		assertTrue(2.0 == IntroductionChallenges.calculator(1,1,"+"));
+////	}
+////
+//
+//}
