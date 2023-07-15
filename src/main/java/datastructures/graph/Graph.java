d42cfe09293b5b0fdddc2141bcd5c9bd7d3e2406
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-07-15 15:07:31 +0300

    create graph

---
+++ src/main/java/datastructures/graph/Graph.java
@@ -0,0 +1,38 @@
+package datastructures.graph;
+
+import java.util.*;
+
+public class Graph {
+    private int numVertices;
+    private LinkedList<Integer>[] adjacencyList;
+
+    public Graph(int numVertices) {
+        this.numVertices = numVertices;
+        adjacencyList = new LinkedList[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination) {
+        adjacencyList[source].add(destination);
+        adjacencyList[destination].add(source);
+    }
+
+
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(10);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+        graph.addEdge(3, 7);
+        graph.addEdge(4, 8);
+        graph.addEdge(5, 9);
+
+    }
+}
