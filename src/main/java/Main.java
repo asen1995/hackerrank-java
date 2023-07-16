500b993af93f8f08d85257aa561438ae907185dd
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-07-16 12:31:17 +0300

    unsafed files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,75 +1,88 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Stack;
 
 public class Main {
 
+
+//    You are given 2 integers. Count the number of all the 1s in the binary representation of the product of two numbers.//
+//    Both integers are non-negative.Example: 3 * 7 = 21 (10101).//
+//    Therefore, the result should be 3.public int countOnes(int a, int b) {}
+
     public static void main(String[] args) {
 
-
-        List<Person> persons = new ArrayList<>();
-
-        for (int i = 0; i < 1_000_000_000; i++) {
-            persons.add(new Person("Name" + i, i));
-        }
-
-        //is blank string
-        String s = " ";
-        System.out.println(s.isBlank());
-
-        long currentTime = System.currentTimeMillis();
-
-        List<PersonDTo> personDTos = persons.stream()
-                .map(person -> new PersonDTo(person.getName(), person.getAge()))
-                .collect(Collectors.toList());
-
-        System.out.println("Stream took: " + (System.currentTimeMillis() - currentTime) + " ms");
-
-
-        currentTime = System.currentTimeMillis();
-
-        List<PersonDTo> personDTos2 = new ArrayList<>();
-
-        for (Person person : persons) {
-            personDTos2.add(new PersonDTo(person.getName(), person.getAge()));
-        }
-
-        System.out.println("For loop took: " + (System.currentTimeMillis() - currentTime) + " ms");
+//        System.out.println(new Main().solution("13 DUP 4 POP 5 DUP + DUP + -"));
+        System.out.println(new Main().solution("5 POP"));
     }
 
-    static class Person {
-        private String name;
-        private int age;
+    public int solution(String S) {
 
-        public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
+        //
+        final String[] sequenceOperations = S.split(" ");
+
+        Stack<Integer> numbersStack = new Stack<>();
+
+        final int machineErrorCode = -1;
+
+        for (String element : sequenceOperations) {
+
+            switch (element.trim()) {
+
+                case "DUP": {
+
+                    if (numbersStack.isEmpty()) {
+                        return machineErrorCode;
+                    }
+                    numbersStack.push(numbersStack.peek());
+
+                    break;
+                }
+                case "POP": {
+                    if (numbersStack.isEmpty()) {
+                        return machineErrorCode;
+                    }
+                    numbersStack.pop();
+
+                    break;
+                }
+                case "+": {
+                    if (numbersStack.size() < 2) {
+                        return machineErrorCode;
+                    }
+
+                    final Integer tail = numbersStack.pop();
+                    final Integer secondToTail = numbersStack.pop();
+
+                    numbersStack.push(tail + secondToTail);
+                    break;
+                }
+                case "-": {
+
+                    if (numbersStack.size() < 2) {
+                        return machineErrorCode;
+                    }
+
+
+                    final Integer tail = numbersStack.pop();
+                    final Integer secondToTail = numbersStack.pop();
+
+                    numbersStack.push(tail - secondToTail);
+
+                    break;
+                }
+
+                default: {
+                    try {
+                        numbersStack.push(Integer.parseInt(element));
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException("Invalid operation " + element);
+                    }
+                }
+
+
+            }
+
+
         }
 
-        public String getName() {
-            return name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-    }
-
-    static class PersonDTo {
-        private String name;
-        private int age;
-
-        public PersonDTo(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getAge() {
-            return age;
-        }
+        return numbersStack.isEmpty() ? machineErrorCode : numbersStack.pop();
     }
 }
