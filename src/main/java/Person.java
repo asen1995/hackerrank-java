361958af543e123c57a80aef6685d937bf37eb46
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-07-19 15:05:34 +0300

    unused methods

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -13,37 +13,11 @@ public class Person implements Serializable, Cloneable {
 		return super.clone();
 	}
 
-	public static void setCountry(String country) {
-		Person.country = country;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 
 	// getters and setters
 }
