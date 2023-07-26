4d9579bd454579afd2e8ebe438472c0d21c9ad4a
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-07-26 15:23:32 +0300

    cleanup

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -88,10 +88,6 @@ public class IntroductionChallenges {
 
     }
 
-    int calculateSum(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        return n + calculateSum(n - 1);
-    }
+
+
 }
