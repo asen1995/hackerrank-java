dce49d6693d9cfa4d1152ea46344301560029f62
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-07-30 14:47:00 +0300

    Revert "removing"
    This reverts commit 370a8106df330a8296af58a1a1f3e34291094bb4.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -69,5 +69,34 @@ public class IntroductionChallenges {
 
     }
 
+    int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+
+    }
+
+    int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+
+    }
+
+
+    short sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (short) (n % 10 + sumOfDigits(n / 10));
+
+    }
+
+
 
 }
