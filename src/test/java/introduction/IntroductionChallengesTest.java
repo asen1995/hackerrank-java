d52ab753bf04f049930874932e5ed7792377f195
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-01 12:02:34 +0300

    remove test

--- src/test/java/introduction/IntroductionChallengesTest.java
+++ src/test/java/introduction/IntroductionChallengesTest.java
@@ -6,17 +6,7 @@ import org.junit.Test;
 
 public class IntroductionChallengesTest {
 
-	@Test
-	public void findDayOfWeek() {
-		String day = IntroductionChallenges.findDayOfWeek(8, 14, 2017);
 
-		assertTrue(day.equals("MONDAY"));
-
-		String test2 = IntroductionChallenges.findDayOfWeek(2, 6, 2022);
-
-		assertTrue(test2.equals("SUNDAY"));
-
-	}
 
 	@Test
 	public void currencyConverter() {
