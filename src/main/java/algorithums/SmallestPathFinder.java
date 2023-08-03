e0be3947c8e01de00f33aad1cc917502be225aea
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-03 16:01:58 +0300

    no comments

--- src/main/java/algorithums/SmallestPathFinder.java
+++ src/main/java/algorithums/SmallestPathFinder.java
@@ -6,23 +6,18 @@ public class SmallestPathFinder {
         final int numberOfRows = matrix.length;
         final int numberOfColumns = matrix[0].length;
 
-        // Create a 2D array to store the minimum path sums
         int[][] arrayPathSums = new int[numberOfRows][numberOfColumns];
 
-        // Initialize the top left element to be the same as the value of the matrix
         arrayPathSums[0][0] = matrix[0][0];
 
-        // Calculate the minimum path sum for the first row
         for (int i = 1; i < numberOfColumns; i++) {
             arrayPathSums[0][i] = arrayPathSums[0][i - 1] + matrix[0][i];
         }
 
-        // Calculate the minimum path sum for the first column
         for (int i = 1; i < numberOfRows; i++) {
             arrayPathSums[i][0] = arrayPathSums[i - 1][0] + matrix[i][0];
         }
 
-        // Calculate the minimum path sum for the rest of the matrix
         for (int row = 1; row < numberOfRows; row++) {
             for (int column = 1; column < numberOfColumns; column++) {
                 arrayPathSums[row][column] =
