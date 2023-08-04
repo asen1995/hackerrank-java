1e91e96b702c79540c147e3493d3478e72a5ef83
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-04 17:32:17 +0300

    remove tests

--- src/test/java/introduction/IntroductionChallengesTest.java
+++ src/test/java/introduction/IntroductionChallengesTest.java
@@ -8,16 +8,5 @@ public class IntroductionChallengesTest {
 
 
 
-	@Test
-	public void currencyConverter() {
-		double payment = 12324.134;
-		assertTrue(IntroductionChallenges.currencyConverter(payment).equals("SUCCESS"));
-	}
-	
-//	@Test
-//	public void calculator() {
-//		assertTrue(2.0 == IntroductionChallenges.calculator(1,1,"+"));
-//	}
-//	
 
 }
