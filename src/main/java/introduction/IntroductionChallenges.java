791863b139e2e063b61874c30b62517a363df0a6
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-10 18:49:15 +0300

    Revert "remove fibonacci"
    This reverts commit 8575f3abf80cd33ee751a59c5056653d73ddaa99.

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -40,6 +40,16 @@ public class IntroductionChallenges {
 
     }
 
+    int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+
+    }
 
     int calculateSum(int n) {
         if (n == 1) {
