c5e12db830cd2bc4ea77bac14f4319b4a3e10516
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-18 16:41:52 +0300

    remove main

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -2,30 +2,10 @@ package introduction;
 
 import model.BracketValidator;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public class IntroductionChallenges {
 
-
-    public static void main(String[] args) {
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://en.wikipedia.org/wiki/"))
-                .build();
-
-        CompletableFuture<HttpResponse<String>> responseFuture =
-                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-
-        responseFuture.thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
-
-    }
 
     public boolean isItValidBracket(String input) {
         BracketValidator bracketValidator = new BracketValidator();
@@ -50,6 +30,9 @@ public class IntroductionChallenges {
         return fibonacci(n - 1) + fibonacci(n - 2);
 
     }
+
+
+
 
     int calculateSum(int n) {
         if (n == 1) {
