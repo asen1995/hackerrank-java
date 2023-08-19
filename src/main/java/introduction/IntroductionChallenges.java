eba3f347f421e2176b24bb302321218d0736cea1
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-19 16:59:50 +0300

    remove main

--- src/main/java/introduction/IntroductionChallenges.java
+++ src/main/java/introduction/IntroductionChallenges.java
@@ -11,22 +11,6 @@ import java.util.concurrent.CompletableFuture;
 
 public class IntroductionChallenges {
 
-
-    public static void main(String[] args) {
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://en.wikipedia.org/wiki/"))
-                .build();
-
-        CompletableFuture<HttpResponse<String>> responseFuture =
-                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-
-        responseFuture.thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
-
-    }
-
     public boolean isItValidBracket(String input) {
         BracketValidator bracketValidator = new BracketValidator();
         return bracketValidator.isValid(input);
