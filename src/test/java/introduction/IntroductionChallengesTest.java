5923aa1bc958fc4d455376300d916640d74ae29c
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-08-24 15:44:51 +0300

    uncommited

--- src/test/java/introduction/IntroductionChallengesTest.java
+++ src/test/java/introduction/IntroductionChallengesTest.java
@@ -1,24 +1,28 @@
 package introduction;
 
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-
 public class IntroductionChallengesTest {
 
-	@Test
-	public void testingValidBrackets() {
-		IntroductionChallenges introductionChallenges = new IntroductionChallenges();
-		final boolean itValidBracket = introductionChallenges.isItValidBracket("()[]{}");
-		assertTrue(itValidBracket);
-	}
+    @Test
+    public void testingValidBrackets() {
+        IntroductionChallenges introductionChallenges = new IntroductionChallenges();
+        final boolean itValidBracket = introductionChallenges.isItValidBracket("()[]{}");
+        assertTrue(itValidBracket);
+    }
 
-	@Test
-	public void testingInvalidBrackets() {
-		IntroductionChallenges introductionChallenges = new IntroductionChallenges();
-		final boolean itValidBracket = introductionChallenges.isItValidBracket("([)]");
-		assertFalse(itValidBracket);
-	}
+    @Test
+    public void testingInvalidBrackets() {
+        IntroductionChallenges introductionChallenges = new IntroductionChallenges();
+        final boolean itValidBracket = introductionChallenges.isItValidBracket("([)]");
+        assertFalse(itValidBracket);
+    }
+
 
 }
