655fdfd28cd4b03549c894474cc55ccb2d366fda
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-09-02 12:21:42 +0300

    Revert "remove comments"
    This reverts commit 652638149d2c3c514572ad22ad6d3c4478b6863e.

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -484,6 +484,9 @@ public class AlgorithmChallenges {
 
     public void bonAppetit(List<Integer> bill, int k, int b) {
 
+        //b - what brian bill Anna
+        //k - what Anna decided not to eat
+
         int annaWillNotEatMealIndex = bill.get(k);
         int sumToBePayedByEachPerson = (bill.stream().mapToInt(Integer::intValue).sum() - annaWillNotEatMealIndex) / 2;
 
