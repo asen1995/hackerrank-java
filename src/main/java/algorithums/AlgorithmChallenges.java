0da711d23140f397434bd8e3349b1297a0854ac5
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-09-09 17:46:48 +0300

    remove

--- src/main/java/algorithums/AlgorithmChallenges.java
+++ src/main/java/algorithums/AlgorithmChallenges.java
@@ -569,22 +569,6 @@ public class AlgorithmChallenges {
         return arr;
     }
 
-    public int[] bubbleSort(int[] arr) {
-
-        for (int i = 0; i < arr.length; i++) {
-
-            for (int j = 0; j < arr.length - i - 1; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-
-        return arr;
-    }
 
     public String reverseStringRecursion(String string) {
 
