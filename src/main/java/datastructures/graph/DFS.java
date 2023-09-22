a48a6be33d7900417a9d3656cb6ea009d0934688
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-09-22 04:03:39 +0300

    adding javadoc

--- src/main/java/datastructures/graph/DFS.java
+++ src/main/java/datastructures/graph/DFS.java
@@ -1,6 +1,19 @@
 package datastructures.graph;
 
+/**
+ * <p>
+ * This is a specific implementation of the GraphSearch interface for DFS
+ * </p>
+ * author: Asen Nikolaev
+ */
 public class DFS implements GraphSearch {
+
+    /**
+     * <p>
+     * This method implements the DFS algorithm
+     * </p>
+     * @param root - the root node of the graph
+     */
     @Override
     public void search(Node root) {
 
--- src/main/java/datastructures/graph/GraphSearch.java
+++ src/main/java/datastructures/graph/GraphSearch.java
@@ -1,6 +1,19 @@
 package datastructures.graph;
 
+/**
+ * <p>
+ * This is graph search interface that all specific implementations should implement
+ * and provide their own implementation of the search method
+ * </p>
+ * author: Asen Nikolaev
+ */
 public interface GraphSearch {
 
+    /**
+     * <p>
+     * This method should be implemented by all specific implementations of the GraphSearch interface
+     * </p>
+     * @param root - the root node of the graph
+     */
     void search(Node root);
 }
