03fcd3f2694de919fa2cf71d4f4d1a763b319ea5
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-09-22 04:04:34 +0300

    javadoc for bfs

--- src/main/java/datastructures/graph/Graph.java
+++ src/main/java/datastructures/graph/Graph.java
@@ -7,7 +7,7 @@ public class Graph {
 
         final Node root = GraphFactory.createGraph();
 
-        GraphSearch search = new DFS();
+        GraphSearch search = new BFS();
 
         search.search(root);
     }
